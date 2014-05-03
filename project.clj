56b46b9741a1099c74ddaf6d47b0d67485ddd448
david.j.palaitis@gmail.com <palaitis@davids-air.home>
2014-05-03 18:34:48 -0400

    initial draft

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject elastic-riemann "0.1.0-SNAPSHOT"
+  :description "A plugin for using ElasticSearch and Kibana with Riemann"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.1"]
+                 [clj-logging-config "1.9.6"]
+                 [riemann "0.2.2"]
+                 [clojurewerkz/elastisch "1.2.0"]
+                 [org.elasticsearch/elasticsearch "0.90.3"]
+                 ]
+  
+  :main elastic-riemann.core)
+
