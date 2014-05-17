42d75172bb2b2d691f8d2ff28ae7b391ed20281a
david.j.palaitis@gmail.com <palaitis@Davids-MacBook-Air.local>
2014-05-17 15:15:43 -0400

    testing

--- src/riemann/core.clj
+++ src/riemann/core.clj
@@ -4,7 +4,10 @@
 ;; get list of modified files
 (defn modified [] (split (:out (sh "git" "ls-files" "-m")) #"\n"))
 
-;; t log --diff-filter=D --summary | grep delete
+;; get list of deleted files
+;; git log --diff-filter=D --summary | grep delete
+
+
 
 
 
