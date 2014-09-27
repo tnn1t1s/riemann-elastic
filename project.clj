daa2d339fbc11fea964b55fd9a2984a291819118
david.j.palaitis@gmail.com <palaitis@Davids-MacBook-Air.local>
2014-09-27 08:42:31 -0400

    exclude log4j and joda-time to silence warnings

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/tools.cli "0.2.1"]
                  [cheshire "5.3.1"]
                  [clj-logging-config "1.9.6"]
-                 [riemann "0.2.6"]
+                 [riemann "0.2.6" :exclusions [joda-time org.slf4j/slf4j-log4j12]]
                  [clojurewerkz/elastisch "1.2.0"]
                  [org.elasticsearch/elasticsearch "0.90.3"]
                  ]
