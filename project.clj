3a747b80e3333022e797ae626de00ca55bc0c9e3
Charles Gruenwald <charles@inboxapp.com>
2016-05-07 09:41:52 -0600

    [deps] Update deps to newest.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject org.tinnitus.dl/elastic-riemann "0.1.1-SNAPSHOT"
   :description "A plugin for using ElasticSearch and Kibana with Riemann"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.1"]
-                 [cheshire "5.3.1"]
-                 [clj-logging-config "1.9.6"]
-                 [riemann "0.2.10" :exclusions [joda-time org.slf4j/slf4j-log4j12]]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [cheshire "5.6.1"]
+                 [clj-logging-config "1.9.12"]
+                 [riemann "0.2.11" :exclusions [joda-time org.slf4j/slf4j-log4j12]]
                  [clojurewerkz/elastisch "1.2.0"]
-                 [org.elasticsearch/elasticsearch "0.90.3"]
+                 [org.elasticsearch/elasticsearch "2.3.2"]
                  ]
   :main elastic-riemann.core)
 
